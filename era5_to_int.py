43162dfc8de367cadc587565b447b01a1732dacd
Michael Duda <duda@ucar.edu>
2025-01-28 13:52:48 -0700

    Add docstrings to era5_to_int.py, and improve line wrapping and indentation

--- era5_to_int.py
+++ era5_to_int.py
@@ -2,6 +2,10 @@
 
 
 def days_in_month(year, month):
+    """ Returns the number of days in a month, depending on the year.
+    A Gregorian calendar is assumed for the purposes of determining leap
+    years.
+    """
     non_leap_year = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
     leap_year     = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
@@ -12,6 +16,10 @@ def days_in_month(year, month):
 
 
 def intdate_to_string(utc_int):
+    """ Converts an integer date representation into a string.
+    The digits of utc_int represent yyyymmddhh, and the returned string
+    is of the form 'yyyy-mm-dd_hh:00:00'.
+    """
     year = utc_int // 1000000
     month = (utc_int // 10000) % 100
     day = (utc_int // 100) % 100
@@ -20,23 +28,44 @@ def intdate_to_string(utc_int):
 
 
 def begin_6hourly(yyyy, mm, dd, hh):
+    """ Returns a date-time string of the form yyyymmddhh for the year, month,
+    day, and hour with the hours rounded down the the beginning of a six-hour
+    interval, i.e., 0, 6, 12, and 18.
+    """
     return f'{yyyy:04d}{mm:02d}{dd:02d}{((hh // 6) * 6):02d}'
 
 
 def end_6hourly(yyyy, mm, dd, hh):
+    """ Returns a date-time string of the form yyyymmddhh for the year, month,
+    day, and hour with the hours rounded up the the last hour of a six-hour
+    interval, i.e., 5, 11, 17, and 23.
+    """
     return f'{yyyy:04d}{mm:02d}{dd:02d}{((hh // 6) * 6 + 5):02d}'
 
 
 def begin_monthly(yyyy, mm, dd, hh):
+    """ Returns a date-time string of the form yyyymmddhh for the year, month,
+    day, and hour with the day and hour rounded down the the beginning of a
+    monthly interval.
+    """
     return f'{yyyy:04d}{mm:02d}{1:02d}{0:02d}'
 
 
 def end_monthly(yyyy, mm, dd, hh):
+    """ Returns a date-time string of the form yyyymmddhh for the year, month,
+    day, and hour with the day and hour rounded up the the last day and hour
+    of the month.
+    """
     return f'{yyyy:04d}{mm:02d}{days_in_month(yyyy,mm):02d}{23:02d}'
 
 
 class MapProjection:
-    def __init__(self, projType, startLat, startLon, startI, startJ, deltaLat, deltaLon,
+    """ Stores parameters of map projections as used in the WPS intermediate
+    file format.
+    """
+
+    def __init__(self, projType, startLat, startLon, startI, startJ,
+                 deltaLat, deltaLon,
                  dx=0.0, dy=0.0, truelat1=0.0, truelat2=0.0, xlonc=0.0):
         self.projType = projType
         self.startLat = startLat
@@ -53,7 +82,12 @@ class MapProjection:
 
 
 class MetVar:
-    def __init__(self, WPSname, ERA5name, ERA5file, beginDateFn, endDateFn, mapProj, isInvariant=False):
+    """ Describes a variable to be converted from netCDF to intermediate file
+    format.
+    """
+
+    def __init__(self, WPSname, ERA5name, ERA5file, beginDateFn, endDateFn,
+                 mapProj, isInvariant=False):
         self.WPSname = WPSname
         self.ERA5name = ERA5name
         self.ERA5file = ERA5file
@@ -64,13 +98,19 @@ class MetVar:
 
 
 def find_glade_file(var, validtime):
+    """ Returns a filename with path information of an ERA5 netCDF file given
+    the ERA5 netCDF variable name and the valid time of the variable.
+    If no file can be found containing the specified variable at the specified
+    time, and empty string is returned.
+    """
     import os.path
 
-    glade_paths = [ '/glade/campaign/collections/rda/data/ds633.6/e5.oper.an.ml/',
-                    '/glade/campaign/collections/rda/data/ds633.0/e5.oper.an.sfc/',
-                    '/glade/campaign/collections/rda/data/ds633.0/e5.oper.invariant/197901/',
-                    '/gpfs/csfs1/collections/rda/decsdata/ds630.0/P/e5.oper.invariant/201601/'
-                  ]
+    glade_paths = [
+        '/glade/campaign/collections/rda/data/ds633.6/e5.oper.an.ml/',
+        '/glade/campaign/collections/rda/data/ds633.0/e5.oper.an.sfc/',
+        '/glade/campaign/collections/rda/data/ds633.0/e5.oper.invariant/197901/',
+        '/gpfs/csfs1/collections/rda/decsdata/ds630.0/P/e5.oper.invariant/201601/'
+        ]
 
     tmp = validtime.split('-')
     yyyy = int(tmp[0])
@@ -96,6 +136,13 @@ def find_glade_file(var, validtime):
 
 
 def find_time_index(ncfilename, validtime):
+    """ Returns a 0-based offset into the unlimited dimension of the specified
+    valid time within the specified etCDF file.
+    If the specified time is not found in the file, an offset of -1 is
+    returned.
+    The 'utc_date' variable is assumed to exist in the netCDF file, and it is
+    through this variable that the search for the valid time takes place.
+    """
     from netCDF4 import Dataset
     import numpy as np
 
@@ -113,19 +160,26 @@ def find_time_index(ncfilename, validtime):
 
         idx = np.where(needed_date == utc_date)
         if idx[0].size == 0:
-            return -1 
+            return -1
         else:
             return idx[0][0]
 
 
 def write_slab(slab, xlvl, proj, WPSname, hdate, units, map_source, desc):
+    """ Writes a 2-d array (a 'slab' of data) to an opened intermediate file
+    using the provided level, projection, and other metadata.
+    This routine assumes that the intermediate file has already been created
+    through a previous call to the WPSUtils.intermediate.write_met_init
+    routine.
+    """
     import WPSUtils
 
-    stat = WPSUtils.intermediate.write_next_met_field(5, slab.shape[0], slab.shape[1],
-            proj.projType, 0.0, xlvl,
-            proj.startLat, proj.startLon, proj.startI, proj.startJ, proj.deltaLat, proj.deltaLon,
-            proj.dx, proj.dy, proj.xlonc, proj.truelat1, proj.truelat2, 6371229.0,
-            0, v.WPSname, hdate, units, map_source, desc, slab)
+    stat = WPSUtils.intermediate.write_next_met_field(
+        5, slab.shape[0], slab.shape[1], proj.projType, 0.0, xlvl,
+        proj.startLat, proj.startLon, proj.startI, proj.startJ,
+        proj.deltaLat, proj.deltaLon, proj.dx, proj.dy, proj.xlonc,
+        proj.truelat1, proj.truelat2, 6371229.0, 0, v.WPSname,
+        hdate, units, map_source, desc, slab)
 
 
 if __name__ == '__main__':
@@ -144,8 +198,11 @@ if __name__ == '__main__':
 
     initdate = sys.argv[1]
 
-    Gaussian = MapProjection(WPSUtils.intermediate.proj_gauss, 89.7848769072, 0.0, 1.0, 1.0, 640.0 / 2.0, 360.0 / 1280.0)
-    LatLon = MapProjection(WPSUtils.intermediate.proj_latlon, 90.0, 0.0, 1.0, 1.0, -0.25, 0.25)
+    # Set up the two map projections used in the ERA5 fields to be converted
+    Gaussian = MapProjection(WPSUtils.intermediate.proj_gauss,
+         89.7848769072, 0.0, 1.0, 1.0, 640.0 / 2.0, 360.0 / 1280.0)
+    LatLon = MapProjection(WPSUtils.intermediate.proj_latlon,
+         90.0, 0.0, 1.0, 1.0, -0.25, 0.25)
 
     int_vars = []
     int_vars.append(MetVar('SPECHUMD', 'Q', 'e5.oper.an.ml.0_5_0_1_0_q.regn320sc.{}_{}.nc', begin_6hourly, end_6hourly, Gaussian))
@@ -192,10 +249,12 @@ if __name__ == '__main__':
                 xlvl = 200100.0
                 if v.WPSname == 'SOILGEO':
                     xlvl = 1.0
-                write_slab(slab, xlvl, proj, v.WPSname, hdate, units, map_source, desc)
+                write_slab(slab, xlvl, proj, v.WPSname, hdate, units,
+                    map_source, desc)
             else:
                 for k in range(f.dimensions['level'].size):
                     slab = np.transpose(field_arr[k,:,:])
-                    write_slab(slab, float(f.variables['level'][k]), proj, v.WPSname, hdate, units, map_source, desc)
+                    write_slab(slab, float(f.variables['level'][k]), proj,
+                        v.WPSname, hdate, units, map_source, desc)
 
     stat = WPSUtils.intermediate.write_met_close()
